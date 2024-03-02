61f1a9aad2764bc19c0627c78f413e8755fe1972
WilfriedNgassarou <wilfriedngassarou23@gmail.com>
2024-03-02 10:32:33 +0100

    Version finale

--- src/components/Credit/Credit.jsx
+++ src/components/Credit/Credit.jsx
@@ -1,8 +1,8 @@
 export default function Credit() {
   return (
     <section className="credit">
-      <span>Designed by <a href="#">Emani Ui</a> </span>
-      <span>Coded by <a href="#">Wilfried Ngassarou</a></span>
+      <span>Designed by <a target="_blank" href="https://twitter.com/EmaniUi">Emani Ui</a> </span>
+      <span>Coded by <a target="_blank" href="https://twitter.com/Wilfried_Ng23">Wilfried Ngassarou</a></span>
     </section>
   )
 }
